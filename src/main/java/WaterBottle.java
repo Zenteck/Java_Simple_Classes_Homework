a59d9faa5ae5be9085aae3da2c0fef3381f6a6bb
Alan <spambox+github@zenteck.co.uk>
2020-01-27 16:05:24 +0000

    WaterBottle done, kindof

---
+++ src/main/java/WaterBottle.java
@@ -0,0 +1,28 @@
+public class WaterBottle {
+
+    public int volume = 100;
+
+    public WaterBottle(int volume) {
+        this.volume = 100;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public int drink() {
+        volume -= 10;
+        return volume;
+    }
+
+    public int empty() {
+        volume -= volume;
+        return volume;
+    }
+
+    public int fill() {
+        volume -= volume;
+        volume += 100;
+        return volume;
+    }
+}
